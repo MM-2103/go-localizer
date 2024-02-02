87c1c9769f1bc35f6fe39bf7724e8f19d5837162
MM-2103 <mohsen.menem1@gmail.com>
2024-02-03 00:56:01 +0100

    added a test to interacting with database and added a query logger

--- go.mod
+++ go.mod
@@ -3,42 +3,16 @@ module github.com/mm-2103/go_localizer
 go 1.21.6
 
 require (
-	cloud.google.com/go/translate v1.10.1
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/zerolog v1.28.0
+	github.com/simukti/sqldb-logger v0.0.0-20230108155151-646c1a075551
+	github.com/simukti/sqldb-logger/logadapter/zerologadapter v0.0.0-20230108155151-646c1a075551
 )
 
 require (
-	cloud.google.com/go v0.112.0 // indirect
-	cloud.google.com/go/compute v1.23.3 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/longrunning v0.5.4 // indirect
-	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
-	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
-	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.47.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.47.0 // indirect
-	go.opentelemetry.io/otel v1.22.0 // indirect
-	go.opentelemetry.io/otel/metric v1.22.0 // indirect
-	go.opentelemetry.io/otel/trace v1.22.0 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/oauth2 v0.16.0 // indirect
-	golang.org/x/sync v0.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/time v0.5.0 // indirect
-	google.golang.org/api v0.160.0 // indirect
-	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/genproto v0.0.0-20240116215550-a9fa1716bcac // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240125205218-1f4bbc51befe // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240116215550-a9fa1716bcac // indirect
-	google.golang.org/grpc v1.61.0 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,22 +2,74 @@ package main
 
 import (
 	"database/sql"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
-
-	"cloud.google.com/go/translate/apiv3"
+	"github.com/rs/zerolog"
+	sqldblogger "github.com/simukti/sqldb-logger"
+	"github.com/simukti/sqldb-logger/logadapter/zerologadapter"
+	// "cloud.google.com/go/translate/apiv3"
 )
+
+// This function handles loading .env and preparing database connection
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 // This function will handle the database connection
 func main() {
-	db, err := sql.Open("mysql", "user:password@/dbname")
+	debug := flag.Bool("debug", false, "Enable debug mode for SQL logging")
+	flag.Parse()
+
+	LoadEnv()
+
+	dbUser := os.Getenv("DATABASE_USER")
+	dbName := os.Getenv("DATABASE_NAME")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	var db *sql.DB
+	var err error
+
+	if *debug {
+		logger := zerolog.New(os.Stdout)
+		loggerAdapter := zerologadapter.New(logger)
+		db, err = sql.Open("mysql", dataSourceName)
+		if err != nil {
+			log.Fatalf("Could not open database: %v", err)
+		}
+		db = sqldblogger.OpenDriver(dataSourceName, db.Driver(), loggerAdapter)
+	} else {
+		db, err = sql.Open("mysql", dataSourceName)
+		if err != nil {
+			log.Fatalf("Could not connect to database: %v", err)
+		}
+	}
+	defer db.Close()
+
+	err = db.Ping()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Could not connect to database: %v", err)
+	} else {
+		fmt.Println("Succesfully connected to database!")
+	}
+
+	err = sendQuery(db)
+	if err != nil {
+		log.Fatalf("Failed to send query: %v", err)
+	} else {
+		fmt.Println("Query executed succesfully!")
 	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -25,7 +77,21 @@ func main() {
 	db.SetMaxIdleConns(10)
 }
 
-// This function handles translation of string through google cloud api
-func translateProducts() {
+func sendQuery(db *sql.DB) error {
+	stmt, err := db.Prepare("SELECT `name` FROM `trrc_product_flat` WHERE `locale` = 'nl';")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
+	_, err = stmt.Exec()
+	if err != nil {
+		return err
+	}
+	return nil
 }
+
+// This function handles translation of string through google cloud api
+// func translateProducts() {
+//
+// }
